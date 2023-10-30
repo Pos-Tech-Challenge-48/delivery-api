214040f2380208def15f817ca8ac35ce6365eac1
Vitormartins12 <vitorsmap@gmail.com>
2023-10-29 23:21:19 -0300

    fix: missing datetime fields

--- internal/adapter/repositories/product.go
+++ internal/adapter/repositories/product.go
@@ -121,10 +121,12 @@ func (r *ProductRepository) GetAll(ctx context.Context, category string) ([]doma
 		SELECT
 			product.product_id,
 			category_name,
-			product_name,
-			product_description,
+			product.product_name,
+			product.product_description,
 			product_unitary_price,
-			COALESCE(product_image.product_image_src_uri, '')
+			COALESCE(product_image.product_image_src_uri, ''),
+			product.created_date_db,
+			product.last_modified_date_db
 		FROM product
         INNER JOIN category
             ON category_id = product_category_id
@@ -153,6 +155,8 @@ func (r *ProductRepository) GetAll(ctx context.Context, category string) ([]doma
 			&productItem.Description,
 			&productItem.Price,
 			&productItem.Image,
+			&productItem.CreatedDate,
+			&productItem.LastModifiedDate,
 		)
 		if err != nil {
 			return result, err
